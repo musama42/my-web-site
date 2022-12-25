e28a684ab881d691a0fce6646e21070e41fb8b5f
Muhammad Usama <usambmuh@corp.contoursoftware.net>
2022-12-25 11:14:31 +0500

    first commit

---
+++ pelindrome.js
@@ -0,0 +1,26 @@
+// program to check if the string is palindrome or not
+
+function checkPalindrome(string) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return alert("It is not a palindrome");
+        }
+    }
+    return alert("It is a palindrome");
+    
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+// call the function
+const value = checkPalindrome(string);
+
+console.log(value);
